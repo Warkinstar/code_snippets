2052e36a33fb8aab0e1bd4fc0d73bbd45fa031af
Ramil <Warkinstar@mail.ru>
2023-07-29 20:59:05 +0600

    Binding ViewSets to URLs explicitly

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,21 +1,60 @@
 from django.urls import path
 from snippets import views
 from rest_framework.urlpatterns import format_suffix_patterns
+from snippets.views import SnippetViewSet, UserViewSet, api_root
+from rest_framework import renderers
 
 
-urlpatterns = [
-    # path("snippets/", views.snippet_list),
-    # path("snippets/<int:pk>/", views.snippet_detail),
-    path("snippets/", views.SnippetList.as_view(), name="snippet-list"),
-    path("snippets/<int:pk>/", views.SnippetDetail.as_view(), name="snippet-detail"),
+# urlpatterns = [
+#     # path("snippets/", views.snippet_list),
+#     # path("snippets/<int:pk>/", views.snippet_detail),
+#     path("snippets/", views.SnippetList.as_view(), name="snippet-list"),
+#     path("snippets/<int:pk>/", views.SnippetDetail.as_view(), name="snippet-detail"),
+#     path("users/", views.UserList.as_view(), name="customuser-list"),
+#     path("users/<int:pk>/", views.UserDetail.as_view(), name="customuser-detail"),
+#     path("", views.api_root),
+#     path(
+#         "snippets/<int:pk>/highlight/",
+#         views.SnippetHighlight.as_view(),
+#         name="snippet-highlight",
+#     ),
+# ]
+#
+# # */snippets.json, */snippets/2.json
+# urlpatterns = format_suffix_patterns(urlpatterns)
 
-    path("users/", views.UserList.as_view(), name="customuser-list"),
-    path("users/<int:pk>/", views.UserDetail.as_view(), name="customuser-detail"),
 
-    path("", views.api_root),
+"""
+    Binding ViewSets to URLs explicitly
+    
+    The handler methods only get bound to the actions when we define the URLConf. 
+    To see what's going on under the hood let's first explicitly create a set of views from our ViewSets.
+"""
 
-    path("snippets/<int:pk>/highlight/", views.SnippetHighlight.as_view(), name="snippet-highlight"),
-]
+snippet_list = SnippetViewSet.as_view(
+    {
+        "get": "list",
+        "post": "create",
+    }
+)
+snippet_detail = SnippetViewSet.as_view(
+    {"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"}
+)
+snippet_highlight = SnippetViewSet.as_view(
+    {"get": "highlight"}, renderer_classes=[renderers.StaticHTMLRenderer]
+)
+user_list = UserViewSet.as_view({"get": "list"})
+user_detail = UserViewSet.as_view({"get": "retrieve"})
 
-# */snippets.json, */snippets/2.json
-urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns = format_suffix_patterns(
+    [
+        path("", api_root),
+        path("snippets/", snippet_list, name="snippet-list"),
+        path("snippets/<int:pk>/", snippet_detail, name="snippet-detail"),
+        path(
+            "snippets/<int:pk>/highlight/", snippet_highlight, name="snippet-highlight"
+        ),
+        path("users/", user_list, name="user-list"),
+        path("users/<int:pk>/", user_detail, name="user-detail"),
+    ]
+)
