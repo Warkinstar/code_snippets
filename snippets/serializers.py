bfe844d47dcd0bab5815b47856747c604a9037a2
Ramil <Warkinstar@mail.ru>
2023-07-29 15:50:53 +0600

    Updating our serializer with read only owner field

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -14,9 +14,13 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+    # owner = serializers.CharField(read_only=True, source="owner.email")
+
+
     class Meta:
         model = Snippet
-        fields = ["id", "title", "code", "linenos", "language", "style"]
+        fields = ["id", "title", "code", "linenos", "language", "style", "owner"]
 
 
 # class SnippetSerializer(serializers.Serializer):
