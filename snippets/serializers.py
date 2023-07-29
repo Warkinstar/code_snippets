e2f9b7692c6a9e5e27ee804463e2f8149ba86aef
Ramil <Warkinstar@mail.ru>
2023-07-29 18:42:14 +0600

    Hyperlinking our API

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,25 +3,62 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth import get_user_model
 
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=Snippet.objects.all()
+"""Hyperlinking API"""
+
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+    highlight = serializers.HyperlinkedIdentityField(
+        view_name="snippet-highlight", format="html"
+    )
+
+    class Meta:
+        model = Snippet
+        fields = [
+            "url",
+            "id",
+            "highlight",
+            "owner",
+            "title",
+            "code",
+            "linenos",
+            "language",
+            "style",
+        ]
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(
+        many=True, view_name="snippet-detail", read_only=True
     )
 
     class Meta:
         model = get_user_model()
-        fields = ["id", "username", "snippets"]
+        fields = ["url", "id", "username", "snippets"]
 
 
-class SnippetSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source="owner.username")
-    # owner = serializers.CharField(read_only=True, source="owner.email")
+"""ModelSerializer class"""
 
+# class UserSerializer(serializers.ModelSerializer):
+#     snippets = serializers.PrimaryKeyRelatedField(
+#         many=True, queryset=Snippet.objects.all()
+#     )
+#
+#     class Meta:
+#         model = get_user_model()
+#         fields = ["id", "username", "snippets"]
+#
+#
+# class SnippetSerializer(serializers.ModelSerializer):
+#     owner = serializers.ReadOnlyField(source="owner.username")
+#     # owner = serializers.CharField(read_only=True, source="owner.email")
+#
+#
+#     class Meta:
+#         model = Snippet
+#         fields = ["id", "title", "code", "linenos", "language", "style", "owner"]
 
-    class Meta:
-        model = Snippet
-        fields = ["id", "title", "code", "linenos", "language", "style", "owner"]
-
+"""Serializer class"""
 
 # class SnippetSerializer(serializers.Serializer):
 #     """Serializer, which can serializing Snippet model"""
